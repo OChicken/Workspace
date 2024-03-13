bf9ceaf7ceaf6e41ebc115edac247372cd796a1a
OChicken <shouran.ma@gmail.com>
2024-03-13 02:29:30 +0100

    delete word

--- .emacs.d/lisp/init-kbd.el
+++ .emacs.d/lisp/init-kbd.el
@@ -155,6 +155,26 @@ With numeric prefix ARG, move current line to window-line ARG."
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+					;             delete word             ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun delete-word (arg)
+  "Delete characters forward until encountering the end of a word.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-region (point) (progn (forward-word arg) (point))))
+
+(defun backward-delete-word (arg)
+  "Delete characters backward until encountering the beginning of a word.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-word (- arg)))
+
+(global-set-key (kbd "C-<backspace>") 'backward-delete-word)  ; origin: backward-kill-word
+(global-set-key (kbd "C-<delete>")             'delete-word)  ; origin:          kill-word
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 					;        Togging among windows        ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
